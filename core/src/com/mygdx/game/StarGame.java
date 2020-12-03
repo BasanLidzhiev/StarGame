b283a70a22f9902598d8c3d70823a0777be571cb
BasanLidzhiev <basan.info.2012@gmail.com>
2020-12-04 02:45:36 +0300

    Отрисовка фона

--- core/src/com/mygdx/game/StarGame.java
+++ core/src/com/mygdx/game/StarGame.java
@@ -5,29 +5,35 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class StarGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    SpriteBatch batch;
+    Texture img;
+    TextureRegion region;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture("picture.png");
+        region = new TextureRegion(img, 100, 100, 150, 150);
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0.85f, 0.23f, 0.76f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
 		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+        batch.setColor(1.35f, 2.3f, 0.55f, 0.5f);
+        batch.draw(img, 300, 300, 200, 200);
+        batch.draw(region, 500, 300, 50, 50);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
 }
